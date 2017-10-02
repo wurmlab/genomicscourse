0c6e6d9e886a5d40278c32f63652048dac5bb00b
Travis CI <travis@bmpvieira.com>
2017-10-02 10:33:01 +0000

    Deploy to GitHub Pages: 8ec8dcd16b8c231475840901ce672d2ffc3db6c4

--- 2017/scripts/setup.sh
+++ 2017/scripts/setup.sh
@@ -3,4 +3,4 @@
 set -x
 cd; mkdir -p hpc; fusermount -u hpc
 sshfs -o follow_symlinks login2.hpc.qmul.ac.uk:/data/home/$USER hpc
-ssh login2.hpc.qmul.ac.uk ln -fs /data/SBCS-MSc-BioInf/data 2017-09-BIO721_genome_bioinformatics_input
+ssh login.hpc.qmul.ac.uk ln -fs /data/SBCS-MSc-BioInf/data 2017-09-BIO721_genome_bioinformatics_input
