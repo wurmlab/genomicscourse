1b0df774f38735494f516190207e1de56ef448a4
Anurag Priyam <a.priyam@icloud.com>
2017-10-02 12:01:33 +0100

    Back to login2

--- 2017/scripts/setup.sh
+++ 2017/scripts/setup.sh
@@ -2,5 +2,5 @@
 # in their HPC home directory (i.e., ~/hpc on the work PC).
 set -x
 cd; mkdir -p hpc; fusermount -u hpc
-sshfs -o follow_symlinks login.hpc.qmul.ac.uk:/data/home/$USER hpc
-ssh login.hpc.qmul.ac.uk ln -fs /data/SBCS-MSc-BioInf/data/ 2017-09-BIO721_genome_bioinformatics_input
+sshfs -o follow_symlinks login2.hpc.qmul.ac.uk:/data/home/$USER hpc
+ssh login2.hpc.qmul.ac.uk ln -fs /data/SBCS-MSc-BioInf/data/ 2017-09-BIO721_genome_bioinformatics_input
